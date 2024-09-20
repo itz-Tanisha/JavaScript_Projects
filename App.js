08786739e9f05d71a638c18312f0d8e1676e9a2a
itz-Tanisha <122715158+itz-Tanisha@users.noreply.github.com>
2024-09-20 13:09:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
+let input = prompt("Enter your name : ");
 
+while(input != "Dheerani"){
+    
+    input = prompt("Enter your correct name : ");
+
+}
+
+alert("Hii Dheerani !!");
+alert("By Tani ♡ ");
