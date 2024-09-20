5691611fc54cb08771b92f2e201b471fdf3c06af
itz-Tanisha <122715158+itz-Tanisha@users.noreply.github.com>
2024-09-20 15:18:39 +0530

    Create App.js

---
+++ NickName/App.js
@@ -0,0 +1,15 @@
+// update the name here 
+
+let name = "Gadhi";
+let myname = "Tani";
+
+let input = prompt("Enter your name : ");
+
+while(input != name){
+    
+    input = prompt("Enter your correct name : ");
+
+}
+
+alert(`Hii ${name} !`);
+alert(`By ${myname} ♡ `);
